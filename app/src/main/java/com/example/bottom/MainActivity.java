5f306d22d9499a4d4d6b349993ba753ae6b62454
arjun3 <arjunalukkal@gmail.com>
2019-06-10 14:23:31 +0530

    latest on monday(10/6/19)

--- app/src/main/java/com/example/bottom/MainActivity.java
+++ app/src/main/java/com/example/bottom/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.widget.NestedScrollView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
@@ -24,13 +26,12 @@ import com.smarteist.autoimageslider.IndicatorAnimations;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     SliderView sliderView;
     ImageView  stack, chat;
     FrameLayout layout;
     NestedScrollView scrollView;
-//    CoordinatorLayout coordinator;
     ConstraintLayout constraintLayout;
     ImageView Register;
     LinearLayout notification;
@@ -41,86 +42,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sliderView = findViewById(R.id.sliderView);
 
+        sliderView = findViewById(R.id.sliderView);
         notification = findViewById(R.id.bottom_notification_li);
-       // notification.setOnClickListener(this);
+        notification.setOnClickListener(this);
         stack = findViewById(R.id.bottom_stack);
         chat = findViewById(R.id.bottom_chat);
-        layout = findViewById(R.id.li_popup);
+        layout = findViewById(R.id.frame_popup);
         scrollView = findViewById(R.id.nested_scroll);
-//        coordinator = findViewById(R.id.coordinator);
         constraintLayout = findViewById(R.id.constraint);
+        constraintLayout.setOnClickListener(this);
         Register = findViewById(R.id.register);
-        //Register.setOnClickListener(this);
+        Register.setOnClickListener(this);
 
         SliderAdapter adapter = new SliderAdapter(this);
         sliderView.setSliderAdapter(adapter);
-
-
 
         sliderView.setIndicatorAnimation(IndicatorAnimations.SWAP); //set indicator animation by using 	  SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
         sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
         sliderView.startAutoCycle();
 
-        notification.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(layout.getVisibility() == View.VISIBLE){
-                    layout.setVisibility(View.GONE);
-
-                } else {
-                    Animation bottomUp = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bottom_up);
-                    layout.startAnimation(bottomUp);
-                    layout.setVisibility(View.VISIBLE);
-
-                }
-
-            }
-        });
-//
-        constraintLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(layout.getVisibility() == View.VISIBLE) {
-                    layout.setVisibility(View.GONE);
-                }
-            }
-        });
-
-
-
-       Register.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, UserRegistration.class);
-                startActivity(intent);
-           }
-       });
-
 
     }
 
 
 
-//    @Override
-//    public void onClick(View v) {
-//        Intent intent;
-//
-//        switch (v.getId()){
-//            case R.id.register:
-//                intent = new Intent(this, UserRegistration.class);
-//                startActivity(intent);
-//                break;
-//
-////            case R.id.bottom_notification_li:
-////                NotificationBottomSheet notificationBottomSheet = new NotificationBottomSheet();
-////                notificationBottomSheet.show(getSupportFragmentManager(), notificationBottomSheet.getTag());
-//
-//
-//
-//        }
-//    }
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        Fragment fragment;
+
+        switch (v.getId()){
+
+            case R.id.register:
+
+                intent = new Intent(this, UserRegistration.class);
+                startActivity(intent);
+                break;
+
+            case R.id.constraint:
+
+                if (layout.getVisibility() == View.VISIBLE){
+                    layout.setVisibility(View.GONE);
+                }
+                break;
+
+            case R.id.bottom_notification_li:
+
+                if (layout.getVisibility() == View.VISIBLE){
+                    layout.setVisibility(View.GONE);
+                } else {
+                    Animation bottomUp = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bottom_up);
+                    layout.startAnimation(bottomUp);
+                    layout.setVisibility(View.VISIBLE);
+                    fragment = new NotificationFragment();
+                    loadFragment(fragment);
+                }
+                break;
+
+
+
+        }
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_popup, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
